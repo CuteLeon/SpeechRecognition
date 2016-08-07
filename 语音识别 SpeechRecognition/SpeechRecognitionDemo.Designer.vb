44852441c78060f71dd8c71748f008c7c2afad2f
Leon <xiaoyankeji@hotmail.com>
2016-08-07 19:06:20 +0800

    第一次提交

---
+++ 语音识别 SpeechRecognition/SpeechRecognitionDemo.Designer.vb
@@ -0,0 +1,90 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class SpeechRecognitionDemo
+    Inherits System.Windows.Forms.Form
+
+    'Form 重写 Dispose，以清理组件列表。
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows 窗体设计器所必需的
+    Private components As System.ComponentModel.IContainer
+
+    '注意: 以下过程是 Windows 窗体设计器所必需的
+    '可以使用 Windows 窗体设计器修改它。  
+    '不要使用代码编辑器修改它。
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.ResultLabel = New System.Windows.Forms.Label()
+        Me.VoiceLevelBar = New System.Windows.Forms.ProgressBar()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'ResultLabel
+        '
+        Me.ResultLabel.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.ResultLabel.Font = New System.Drawing.Font("微软雅黑", 15.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(134, Byte))
+        Me.ResultLabel.Location = New System.Drawing.Point(0, 24)
+        Me.ResultLabel.Name = "ResultLabel"
+        Me.ResultLabel.Size = New System.Drawing.Size(378, 143)
+        Me.ResultLabel.TabIndex = 3
+        Me.ResultLabel.Text = "— —"
+        Me.ResultLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'VoiceLevelBar
+        '
+        Me.VoiceLevelBar.Dock = System.Windows.Forms.DockStyle.Top
+        Me.VoiceLevelBar.Location = New System.Drawing.Point(0, 0)
+        Me.VoiceLevelBar.Name = "VoiceLevelBar"
+        Me.VoiceLevelBar.Size = New System.Drawing.Size(378, 24)
+        Me.VoiceLevelBar.TabIndex = 2
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(303, 30)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 40)
+        Me.Button1.TabIndex = 4
+        Me.Button1.Text = "激活"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(303, 76)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 40)
+        Me.Button2.TabIndex = 5
+        Me.Button2.Text = "关闭"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'SpeechRecognitionDemo
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(378, 167)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.ResultLabel)
+        Me.Controls.Add(Me.VoiceLevelBar)
+        Me.Name = "SpeechRecognitionDemo"
+        Me.ShowIcon = False
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Speech Recognition Demo"
+        Me.TopMost = True
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents ResultLabel As Label
+    Friend WithEvents VoiceLevelBar As ProgressBar
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+End Class
---
+++ 语音识别 SpeechRecognition/SpeechRecognitionDemo.vb
@@ -0,0 +1,114 @@
+﻿Imports System.ComponentModel
+Imports System.Speech.Recognition
+
+Public Class SpeechRecognitionDemo
+    Dim MySpeechRecognitionEngine As SpeechRecognitionEngine = Nothing
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        '初始化引擎
+        MySpeechRecognitionEngine = CreateSpeechEngine("zh-CN")
+
+        '注册事件
+        AddHandler MySpeechRecognitionEngine.AudioLevelUpdated, AddressOf engine_AudioLevelUpdated
+        AddHandler MySpeechRecognitionEngine.SpeechRecognized, AddressOf engine_SpeechRecognized
+        AddHandler MySpeechRecognitionEngine.AudioStateChanged, AddressOf engine_AudioStateChanged
+
+        '导入语法
+        LoadGrammar(Environment.CurrentDirectory + "\\Grammar.txt")
+
+        '使用系统默认麦克风
+        MySpeechRecognitionEngine.SetInputToDefaultAudioDevice()
+
+        '开始监听
+        MySpeechRecognitionEngine.RecognizeAsync(RecognizeMode.Multiple)
+    End Sub
+
+    '创建引擎
+    Private Function CreateSpeechEngine(ByVal PreferredCulture As String) As SpeechRecognitionEngine
+        Dim TempSpeechRecognitionEngine As SpeechRecognitionEngine = Nothing
+        For Each Config As RecognizerInfo In SpeechRecognitionEngine.InstalledRecognizers()
+            '遍历所有引擎语言匹配到指定语言，创建引擎，跳出
+            If (Config.Culture.ToString() = PreferredCulture) Then TempSpeechRecognitionEngine = New SpeechRecognitionEngine(Config) : Exit For
+        Next
+
+        '未匹配到指定引擎，使用默认语言创建语音引擎
+        If (TempSpeechRecognitionEngine Is Nothing) Then
+            Dim tempPreferredCulture As String = SpeechRecognitionEngine.InstalledRecognizers().First.Culture.ToString()
+            MsgBox("未安装指定语言，引擎将会使用默认语言： " & tempPreferredCulture, 64, "未找到指定语言")
+            TempSpeechRecognitionEngine = New SpeechRecognitionEngine(SpeechRecognitionEngine.InstalledRecognizers().First)
+        End If
+
+        Return TempSpeechRecognitionEngine
+    End Function
+
+    '为语音引擎导入语法
+    Private Sub LoadGrammar(Optional ByVal GrammarFilePath As String = vbNullString)
+        '——组合语法：————————————————————————
+        '——每个组合里只能取一个值，顺序从A到C，必须每个组合都有值———
+        'Dim Grammars As GrammarBuilder = New GrammarBuilder()
+        'Grammars.Append(New Choices("汽车", "飞机"))
+        'Grammars.Append(New Choices("前进", "后退"))
+        'Dim GrammarList As Grammar = New Grammar(Grammars)
+        'MySpeechRecognitionEngine.LoadGrammar(GrammarList)
+
+        '——单词语法：————————————————————————
+        'Dim Grammars As GrammarBuilder = New GrammarBuilder()
+        'Grammars.Append(New Choices("汽车", "飞机"))
+        'Dim GrammarList As Grammar = New Grammar(Grammars)
+        'MySpeechRecognitionEngine.LoadGrammar(GrammarList)
+
+        '——从文本读取语法：—————————————————————
+        If GrammarFilePath <> vbNullString And IO.File.Exists(GrammarFilePath) Then
+            '有可用的语法文件，则读取规定的语法
+            Dim Grammars As Choices = New Choices()
+            Dim Lines() As String = IO.File.ReadAllLines(GrammarFilePath, System.Text.Encoding.Default)
+            For Each Line As String In Lines
+                Grammars.Add(Line)
+            Next
+            Dim GrammarList As Grammar = New Grammar(New GrammarBuilder(Grammars))
+            MySpeechRecognitionEngine.LoadGrammar(GrammarList)
+        Else
+            '没有可用的语法，则使用桌面语音技术提供的默认语法
+            MySpeechRecognitionEngine.LoadGrammar(New DictationGrammar)
+        End If
+    End Sub
+
+    '采用与其加载启用的 Grammar 对象匹配的输入
+    Private Sub engine_SpeechRecognized(sender As Object, e As SpeechRecognizedEventArgs)
+        ResultLabel.ForeColor = Color.Black
+        ResultLabel.Text = e.Result.Text
+    End Sub
+
+    '报告音频输入的级别
+    Private Sub engine_AudioLevelUpdated(sender As Object, e As AudioLevelUpdatedEventArgs)
+        VoiceLevelBar.Value = e.AudioLevel
+    End Sub
+
+    '接收的音频更改
+    Private Sub engine_AudioStateChanged(sender As Object, e As AudioStateChangedEventArgs)
+        If e.AudioState = AudioState.Speech Then
+            ResultLabel.ForeColor = Color.DarkGray
+            Me.Text = "语音引擎：Speech..."
+        ElseIf e.AudioState = AudioState.Silence Then
+            Me.Text = "语音引擎：Silence..."
+        Else
+            Me.Text = "语音引擎：Stoped..."
+        End If
+    End Sub
+
+    '程序关闭，停止监听，释放内存
+    Private Sub SpeechRecognitionDemo_Closing(sender As Object, e As CancelEventArgs) Handles Me.Closing
+        MySpeechRecognitionEngine.RecognizeAsyncStop()
+        MySpeechRecognitionEngine.Dispose()
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        On Error Resume Next
+        MySpeechRecognitionEngine.RecognizeAsync(RecognizeMode.Multiple)
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        On Error Resume Next
+        MySpeechRecognitionEngine.RecognizeAsyncStop()
+    End Sub
+End Class
